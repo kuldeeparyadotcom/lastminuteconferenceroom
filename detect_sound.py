5b7050d79270ebdf8b005b3f76edf3e5e7607e88
KD <kuldeeparyadotcom@gmail.com>
2017-12-07 17:12:12 -0600

    first working prototype

---
+++ detect_sound.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Purpose - To demonstrate how sound sensor can be used to detect if room is occupied or not
+Logic - 
+If no sound detected for last 10 minutes, mark the room available (configurable)
+"""
+
+import RPi.GPIO as GPIO
+import time
+
+channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+#Logic to detect if someone is really in meeting room
+from datetime import datetime
+
+tracker = {} #Empty dict for tracking
+tracker["last_time_sound_detected"] = datetime.now() #Originally to start
+def callback(channel):
+    print("Sound detected")
+    tracker["last_time_sound_detected"] = datetime.now() #Capture current timestamp
+    
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) #Detect when pin goes high or low
+GPIO.add_event_callback(channel, callback) #Assign function to GPIO Pin, Run function on change
+
+#Infinite Loop
+while True:
+    #poll every 10 seconds - It is configurable
+    diff = datetime.now() - tracker["last_time_sound_detected"]
+    silence_duration = diff.seconds
+    print("Room is found silent for", silence_duration)
+    time.sleep(10)
